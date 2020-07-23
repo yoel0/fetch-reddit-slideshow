12285b2969183ec1feb5b1de6a1639e2e1970251
yoel <yoel@macbook-pro.home>
2020-07-22 23:47:27 -0700

    [RedditSlider] Add HTML and FETCH

---
+++ js/app.js
@@ -0,0 +1,30 @@
+document.addEventListener("DOMContentLoaded", function () {
+  const requestURL = "https://www.reddit.com/search.json?q=cowboy_bebop";
+
+  fetch(requestURL)
+    .then(function (responseData) {
+      console.log(responseData);
+      return responseData.json();
+    })
+    .then(function (jsonData) {
+      console.log(jsonData);
+
+      let results = jsonData.data.children;
+      let infoNeeded = results.map(function (imageResults) {
+        let image = imageResults.data.thumbnail;
+        if (image.includes("http")) {
+          return image;
+        }
+      });
+      const imageShow = document.getElementById("pain");
+      infoNeeded.forEach((image) => {
+        let imageDaddy = document.createElement("img");
+        imageDaddy.src = `${image}`;
+        imageShow.appendChild(imageDaddy);
+      });
+    });
+});
+
+/// make button function
+/// style my css
+/// figure out to display one image
