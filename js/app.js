bc6142568d080b1c41cb8f90bdbb36bd7c21a977
yoel <yoel@macbook-pro.home>
2020-07-25 01:33:20 -0700

    [RedditSlideShow] add styles

--- js/app.js
+++ js/app.js
@@ -1,29 +1,59 @@
-document.addEventListener("DOMContentLoaded", function () {
-  const requestURL = "https://www.reddit.com/search.json?q=rubix";
+let slideshow;
+let searchForm;
+let input;
+let baseEndpoint = "https://www.reddit.com/search.json?q=";
+let resultsArray = [];
+let currIndex = 0;
+let resetButton;
 
-  fetch(requestURL)
-    .then(function (responseData) {
-      if (responseData.status === 200) return responseData.json();
-      console.log(responseData);
+const changePhoto = () => {
+  if (resultsArray[currIndex + 1].data.thumbnail) {
+    currIndex++;
+  } else {
+    currIndex = 0;
+  }
+  slideshow.setAttribute("src", resultsArray[currIndex].data.thumbnail);
+};
+const startSlideshow = () => {
+  console.log("starting slideshow");
+  // start changing image every 3 seconds
+  setInterval(changePhoto, 3000);
+};
+const getSearchResults = () => {
+  // fetch from the reddit API
+  fetch(`${baseEndpoint}${input.value}`)
+    .then((response) => {
+      return response.json();
     })
-    .then(function (jsonData) {
-      console.log(jsonData);
-
-      let results = jsonData.data.children;
-      let infoNeeded = results.map(function (imageResults) {
-        let image = imageResults.data.thumbnail;
-        return image;
-      });
-
-      const imageShow = document.getElementById("painindass");
-      infoNeeded.forEach((image) => {
-        let imageDaddy = document.createElement("img");
-        imageDaddy.src = `${image}`;
-        imageShow.appendChild(imageDaddy);
-      });
+    .then((jsonData) => {
+      resultsArray = jsonData.data.children;
+      slideshow.setAttribute("src", resultsArray[0].data.thumbnail);
+      startSlideshow();
+    })
+    .catch((err) => {
+      console.log("there was an error fetching the results");
+      console.log(err);
     });
+};
+document.addEventListener("DOMContentLoaded", () => {
+  slideshow = document.getElementById("slideshow");
+  searchForm = document.querySelector("form");
+  input = document.querySelector("input");
+  resetButton = document.getElementById("reset");
+  searchForm.addEventListener("submit", (e) => {
+    e.preventDefault();
+    // slideshow appears
+    slideshow.style.display = "block";
+    // hide the search form
+    searchForm.style.display = "none";
+    // get search results
+    getSearchResults();
+  });
+  resetButton.addEventListener("click", () => {
+    // hide slideshow
+    slideshow.style.display = "none";
+    // show search form
+    searchForm.style.display = "block";
+    resultsArray = [];
+  });
 });
-
-// picture hide / rotate
-// make input field searchable
-// make button function go and stop
